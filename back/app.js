4dd5070919e220daf2a46429b0a093dddbb12b83
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-10 11:28:38 -0300

    feat: updaye app file

--- back/app.js
+++ back/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks.js');
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('\n-------------------------------');
+    console.log(`\nServer is running on port ${port}`);
+    console.log('\n-------------------------------');
+});
