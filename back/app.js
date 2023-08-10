7509bfe98416d6d3b69691afba6c815861430aa3
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-10 11:40:50 -0300

    feat: setup middleware

--- back/app.js
+++ back/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks.js');
 
+// Middleware
+app.use(express.json());
+
 // Routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App');
@@ -12,7 +15,7 @@ app.use('/api/v1/tasks', tasks);
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('\n-------------------------------');
-    console.log(`\nServer is running on port ${port}`);
-    console.log('\n-------------------------------');
+    console.log('\x1b[31m' + '\n-------------------------------');
+    console.log('\x1b[34m' + `\nServer is running on port ${port}`);
+    console.log('\x1b[31m' + '\n-------------------------------');
 });
