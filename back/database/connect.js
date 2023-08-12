4337510db90a163d8008f19b10422a716c0af273
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-11 21:43:19 -0300

    feat: connect on mongoose database

--- back/database/connect.js
+++ back/database/connect.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
 const connectionString = 'Do not hard code !';
+
+mongoose
+    .connect(connectionString)
+    .then(() => {
+        console.log('\x1b[34m' + '\n-------------------------------');
+        console.log('\x1b[31m' + `\nConnected to MongoDB`);
+        console.log('\x1b[34m' + '\n-------------------------------');
+    })
+    .catch((err) => console.log(err));
