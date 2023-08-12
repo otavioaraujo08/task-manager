2d3be599578b45fd8455eb58d7bfedff822ea10a
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-08-11 22:00:58 -0300

    fix: refactor start server functions

--- back/app.js
+++ back/app.js
@@ -1,8 +1,8 @@
-require('./database/connect.js');
-
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks.js');
+const connectDB = require('./database/connect.js');
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
@@ -16,8 +16,21 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log('\x1b[31m' + '\n-------------------------------');
-    console.log('\x1b[34m' + `\nServer is running on port ${port}`);
-    console.log('\x1b[31m' + '\n-------------------------------');
-});
+const startServer = () => {
+    return app.listen(port, () => {
+        console.log('\x1b[31m' + '\n-------------------------------');
+        console.log('\x1b[34m' + `\nServer is running on port ${port}`);
+        console.log('\x1b[31m' + '\n-------------------------------');
+    });
+};
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        startServer();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
